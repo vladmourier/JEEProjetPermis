06ad711cd8c132a827b28133bcde57b0a0fb3cda
vladmourier <vladmourier@hotmail.fr>
2016-05-11 11:42:45 +0200

    initialisation projet

--- src/controller/BaseController.java
+++ src/controller/BaseController.java
@@ -1,5 +1,25 @@
 package controller;
 
-public class BaseController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+@Controller
+public class BaseController extends MultiActionController {
+
+	@RequestMapping(value= "index.htm")
+	public ModelAndView index(HttpServletRequest request, HttpServletResponse response) throws Exception
+	{
+		System.out.println("TEST JE PASSE BIEN DANS LA FONCTION");
+		System.out.println("TEST JE PASSE BIEN DANS LA FONCTION");
+		System.out.println("TEST JE PASSE BIEN DANS LA FONCTION");
+		System.out.println("TEST JE PASSE BIEN DANS LA FONCTION");
+		System.out.println("TEST JE PASSE BIEN DANS LA FONCTION");
+		return new ModelAndView("index");
+	}
 }
